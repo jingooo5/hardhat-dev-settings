7a33addf604cd6c8d66d3c8ebb38c5249c9011d3
jw <jingooo2@g.skku.edu>
2022-06-15 09:56:56 +0900

    update script

--- script/npm-update.sh
+++ script/npm-update.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
+git checkout latest
 line=$(ncu | wc -l)
 
 if [ $line -gt 4 ]
 then
-    git checkout latest
+
     log=$(ncu -u)
 
     FILENAME=$(date +%Y%m%d%H%M%S).txt
     echo ${log} >> ~/Documents/GitHub/hardhat-dev-settings/logs/${FILENAME}
 
+    git add .
+    git commit -m "update script"
+    git push
+    git checkout main
 else
     echo "already updated"
 fi
